7061424b2cfee62b2167347feda439c74db282fe
Made Supardi <grymore@gmail.com>
2024-06-03 10:58:53 +0800

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    url: 'https://mpc-sample.vercel.app/',
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
--- src/app/auth0.tsx
+++ src/app/auth0.tsx
@@ -4,11 +4,12 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 
 export function Providers({ children }: any) {
+  console.log("@process.env.url", process.env.url)
   return (
     <Auth0Provider
       clientId="uEXiWJylfxymCWUwSNl5Z"
       domain="https://auth-wallet-poc.dev.upbond.io"
-      authorizationParams={{ redirect_uri: window.location.origin, scope: "openid profile" }}>
+      authorizationParams={{ redirect_uri: process.env.url, scope: "openid profile" }}>
       {children}
     </Auth0Provider>
   );
